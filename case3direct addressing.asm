38c57b56345eb050305a76261180370593b34db4
sharan1711 <asekarsharan2003@gmail.com>
2026-02-03 22:55:39 +0530

    Add files via upload

---
+++ case3direct addressing.asm
@@ -0,0 +1,5 @@
+ORG 0000H
+
+MOV A, 30H      ; Directly read contents of 30H
+
+END
---
+++ case3indirect addressing.asm
@@ -0,0 +1,6 @@
+ORG 0000H
+
+MOV R0, 30H     ; R0 = 40H (pointer)
+MOV A, @R0      ; Fetch data from address 40H
+
+END
